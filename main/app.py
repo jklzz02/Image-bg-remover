0667cc5218832334195c862988fef24279513ab9
jklzz02 <jacopo.lazzaro@gmail.com>
2025-05-21 23:29:23 +0200

    drop functionality implemented

--- main/app.py
+++ main/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
 from PIL import Image, UnidentifiedImageError
 from rembg import remove
 import io
--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,6 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fileInput = document.getElementById("image_file");
   const fileNameDisplay = document.getElementById("file-name-display");
+  const dropZone = document.getElementById("drop-container");
+
+  dropZone.addEventListener("dragover", (event) => {
+    event.preventDefault();
+  });
+
+  dropZone.addEventListener("drop", (event) => {
+    event.preventDefault();
+
+    const files = event.dataTransfer.files;
+
+    if (files.length === 1) {
+      const file = files[0];
+
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(file);
+      fileInput.files = dataTransfer.files;
+
+      fileNameDisplay.textContent = file.name;
+    } else {
+      fileNameDisplay.textContent = "Please drop only one file.";
+    }
+  });
 
   fileInput.addEventListener("change", () => {
     if (fileInput.files.length > 0) {
